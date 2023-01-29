dda9de6af69b80eadfc57bb913a82a3a837f084f
Leonard Gerschel <leonard.gerschel@uni-bonn.de>
2023-01-29 18:42:14 +0100

    add plugin load

--- Plugin.cs
+++ Plugin.cs
@@ -1,7 +1,23 @@
-﻿namespace coin_pocket_escape
+﻿using PluginAPI.Core.Attributes;
+using PluginAPI.Enums;
+
+namespace coin_pocket_escape
 {
     public class Plugin
     {
+        public static Plugin Singleton;
+
+        [PluginConfig] public Config Config;
+
+        public const string Version = "0.0.1";
         
+        [PluginPriority(LoadPriority.Highest)]
+        [PluginEntryPoint("Coin-Pocket-Escape", Version,
+            "Players have a chance of escaping the pocket dimension when flipping a coin", "Various")]
+        void LoadPlugin()
+        {
+            Singleton = this;
+            PluginAPI.Events.EventManager.RegisterEvents(this);
+        }
     }
 }
