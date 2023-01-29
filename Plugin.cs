576c07a43690ca26b76824ecb5605f3b527419b1
Leonard Gerschel <leonard.gerschel@uni-bonn.de>
2023-01-29 21:48:21 +0100

    finally

--- Plugin.cs
+++ Plugin.cs
@@ -1,6 +1,13 @@
-﻿using PluginAPI.Core;
+﻿using System.Linq;
+using System.Security.Policy;
+using PluginAPI.Core;
 using PluginAPI.Core.Attributes;
+using PluginAPI.Core.Items;
 using PluginAPI.Enums;
+using PluginAPI.Core.Items;
+using PluginAPI.Core.Zones;
+using PluginAPI.Core.Zones.Pocket;
+using PluginAPI.Core.Zones.Pocket;
 using InventorySystem.Items;
 
 namespace coin_pocket_escape
@@ -23,14 +30,15 @@ namespace coin_pocket_escape
         }
         
         [PluginEvent(ServerEventType.PlayerCoinFlip)]
-        public void OnPlayerCoinFlip(MyPlayer player, bool isTails)
+        public void OnPlayerCoinFlip(PluginAPI.Core.Player player, bool isTails)
         {
             Log.Info($"&rPlayer &6{player.Nickname}&r (&6{player.UserId}&r) flipped the coin. Flip result: " +
                      $"{(isTails ? "tails" : "heads")}.");
 
+            if (player.Zone == PluginAPI.Core.Zones.UnknownZone); 
             if (isTails)
             {
-                
+                player.RemoveItem(player.CurrentItem);
             }
         }
     }
