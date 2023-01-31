184ec3462716635c081f0629003a365d7c46ff8e
Leonard Gerschel <leonard.gerschel@uni-bonn.de>
2023-01-31 14:42:52 +0100

    Fix exploit, add text hints

--- Config.cs
+++ Config.cs
@@ -5,6 +5,6 @@ namespace coin_pocket_escape
     public class Config
     {
         [Description("Set the waiting time after the coin is flipped in ms")]
-        public int WaitingTime { get; set; } = 2500;
+        public int WaitingTime { get; set; } = 2000;
     }
 }
