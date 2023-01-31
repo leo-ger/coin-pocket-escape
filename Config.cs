a613bec6ca7e05c4110d2b4e0e74de24d7dc445f
Leonard Gerschel <leonard.gerschel@uni-bonn.de>
2023-01-31 15:21:26 +0100

    Fix void teleports, add randomization feature

--- Config.cs
+++ Config.cs
@@ -6,5 +6,8 @@ namespace coin_pocket_escape
     {
         [Description("Set the waiting time after the coin is flipped in ms")]
         public int WaitingTime { get; set; } = 2000;
+
+        [Description("Toggle coin success randomization")]
+        public bool CoinRandom { get; set; } = true;
     }
 }
